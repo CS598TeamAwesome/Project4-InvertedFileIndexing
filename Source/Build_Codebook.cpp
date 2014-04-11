c2b87e5f64ce7149249910e68b258fb23dca5418
SoulCalculator <sjzezdean@gmail.com>
2014-04-11 00:52:07 -0400

    naive codebook generator

---
+++ Source/Build_Codebook.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <Util\Distances.hpp>
+
+
+std::vector<std::string> load_images(std::string imageFileList)
+{
+	std::ifstream iss(imageFileList);
+
+	std::vector<std::string> images;
+	while (iss)
+	{
+		std::string imFile;
+		std::getline(iss, imFile);
+
+		if (imFile.length() != 0)
+			images.push_back(imFile);
+	}
+
+	iss.close();
+	return images;
+}
+
+int main()
+{
+	std::vector<std::string> filename;
+
+	filename = load_images("all_images");
+	std::cout << "The size of Filelist is: " << filename.size() << std::endl;
+
+	cv::SiftDescriptorExtractor extractor;
+	cv::SiftFeatureDetector detector(200);
+	std::vector<cv::Mat> training_descriptors;
+	std::vector<std::vector<double>> samples;
+	for (int i = 0; i < 50; i++)
+	{
+		cv::Mat img;
+		std::cout << filename[i] << ": ";
+		img = cv::imread(filename[i], CV_LOAD_IMAGE_UNCHANGED);
+		std::vector<cv::KeyPoint> training_keypoints;
+		detector.detect(img, training_keypoints);
+		std::cout << training_keypoints.size() << std::endl;
+
+		cv::Mat descriptor_uchar;
+		extractor.compute(img, training_keypoints, descriptor_uchar);
+
+		cv::Mat descriptor_double;
+		descriptor_uchar.convertTo(descriptor_double, CV_64F);
+
+		training_descriptors.push_back(descriptor_double);
+
+		LocalDescriptorAndBagOfFeature::convert_mat_to_vector(descriptor_double, samples);
+
+	}
+	return 0;
+}
